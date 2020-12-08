3ebe7263cbe139356fc24ccf02b10fa37be0f36e
44sisi <luo.siwei@u.nus.edu>
2020-12-09 00:42:38 +0800

    updated week9-flask with diff hello versions

---
+++ week9-flask/hello2/application.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/greet", methods=["POST"])   # default is "GET"
+def greet():
+    return render_template("greet.html", showname=request.form.get("inputname", "world"))
+
+# to run:
+# export FLASK_APP=application.py OR set FLASK_APP=application.py
+# flask run
---
+++ week9-flask/hello3/application.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template("index.html")
+    elif request.method == "POST":
+        return render_template("greet.html", showname=request.form.get("inputname", "world"))
+
+
+# to run:
+# export FLASK_APP=application.py OR set FLASK_APP=application.py
+# flask run
