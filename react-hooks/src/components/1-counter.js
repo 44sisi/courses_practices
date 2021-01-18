d89ae1c7ffdde9949f44278a51fa4d5f8ef546a3
44sisi <luo.siwei@u.nus.edu>
2021-01-18 21:59:07 +0800

    hooks at a glance: side effect

--- react-hooks/src/components/1-counter.js
+++ react-hooks/src/components/1-counter.js
@@ -1,13 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Counter() {
   // Declare a new state variable, which we'll call "count"
   const [count, setCount] = useState(0);
 
+  useEffect(() => {
+    document.title = `you clicked ${count} times`;
+  });
+
   return (
     <div>
       <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>Click me</button>
+      <button onClick={() => setCount(0)}>Reset</button>
     </div>
   );
 }
