328c8e860e5ab0aa7aeb6a0c5492f26f4c44f42f
44sisi <luo.siwei@u.nus.edu>
2021-01-18 21:29:37 +0800

    added practice for react hooks

---
+++ react-hooks/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Counter from "./components/1-counter.js";
+
+function App() {
+  return (
+    <ol className="App">
+      <li className="Counter">
+        <Counter />
+      </li>
+    </ol>
+  );
+}
+
+export default App;
