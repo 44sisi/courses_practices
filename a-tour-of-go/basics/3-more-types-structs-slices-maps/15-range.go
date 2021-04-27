185d3ccd797cf3907c1c1836ec80e5741ed09cef
44sisi <luo.siwei@u.nus.edu>
2021-04-27 21:17:33 +0800

    add slice exercise

--- a-tour-of-go/basics/3-more-types-structs-slices-maps/15-range.go
+++ a-tour-of-go/basics/3-more-types-structs-slices-maps/15-range.go
@@ -8,4 +8,13 @@ func main() {
 	for i, v := range power {
 		fmt.Printf("2**%d=%d\n", i, v)
 	}
+
+	arr := make([]int, 10)
+	for i:= range arr {
+		arr[i] = 1 << uint(i) // 2**i
+	}
+
+	for _, v := range arr {
+		fmt.Printf("%d\n", v)
+	}
 }
