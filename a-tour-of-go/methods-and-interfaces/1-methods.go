e3de5e46b74a25bc3b398b21ecd8cb0ff59847bd
44sisi <luo.siwei@u.nus.edu>
2021-05-06 23:33:45 +0800

    update method practice

--- a-tour-of-go/methods-and-interfaces/1-methods.go
+++ a-tour-of-go/methods-and-interfaces/1-methods.go
@@ -13,7 +13,13 @@ func (v Vertex) Abs() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
+func NewAbs(v Vertex) float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+
 func main() {
 	v := Vertex{3,4}
 	fmt.Println(v.Abs())
+	fmt.Println(NewAbs(v))
 }
