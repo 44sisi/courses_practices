3b99482e20c02482d3ec5b4bae89105e1c9e3cd0
44sisi <luo.siwei@u.nus.edu>
2022-02-05 00:41:03 +0800

    rename

--- udemy-advanced-web-developer-bootcamp/section6_async_foundations/5-event-loop-and-queue.js
+++ udemy-advanced-web-developer-bootcamp/section6_async_foundations/5-event-loop-and-queue.js
@@ -3,7 +3,7 @@ function square(n) {
 }
 
 setTimeout(() => {
-  console.log('Callback was places on the queue'); // only get from the queue when stack is empty, after console.log(square(2)) is executed
+  console.log('Callback was placed on the queue'); // only get from the queue when stack is empty, after console.log(square(2)) is executed
 }, 0); // even though this call back funciton is set to be run after 0 seconds, 0 means it will be placed in the queue 0 secons from now
 
 console.log(square(2));
